6b6dd0398a5a384a01342254a3452123e9294df9
slab8711 <scottlab@gmail.com>
2026-06-22 14:54:58 -0600

    JS Complete

--- books.js
+++ books.js
@@ -1,7 +1,15 @@
+let books;
+
 async function renderBooks(filter) {
     const booksWrapper = document.querySelector(".books");
     
-    const books = await getBooks();
+    booksWrapper.classList += ' books__loading'
+    
+    if (!books) {
+        books = await getBooks();
+    }
+    
+    booksWrapper.classList.remove('books__loading')
 
     if (filter === 'LOW_TO_HIGH') {
         books.sort((a, b) => a.originalPrice - b.originalPrice);
